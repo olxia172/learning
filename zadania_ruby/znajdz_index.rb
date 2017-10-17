72d95f27fe646f5f2d59b723946aece6dc95d9b7
olxia172 <kucharczyk.aleksandra@gmail.com>
2017-10-17 17:47:32 +0200

    inne podejscie do find_index

--- zadania_ruby/znajdz_index.rb
+++ zadania_ruby/znajdz_index.rb
@@ -5,17 +5,23 @@
 #puts ' '
 
 def find_index(array, value)
-  if array.include?(value)
+  #if array.include?(value)
 
     index = 0
-    while array[index] != value
+    while index < array.size
+      if array[index] == value
+        return index
+      end
       index += 1
     end
-    puts index
+    return 'not found'
 
-  else
-    puts 'not found'
-  end
+  #else
+  #  puts 'not found'
+  #end
 end
 
-find_index([1, 5, 9, 4, 6], 5)
+puts find_index([1, 2, 3, 4, 5], 3)
+puts find_index([5, 8, 6, 2, 2, 10], 2)
+puts find_index([10, 20, 30], 100)
+puts find_index([], 0)
