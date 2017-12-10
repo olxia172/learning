e97959e3a8f5f8704fd515534cc0c8da2534bb2f
olxia172 <kucharczyk.aleksandra@gmail.com>
2017-12-10 13:56:29 +0100

    Add new task

--- zadania_rails/to_do/app/controllers/tasks_controller.rb
+++ zadania_rails/to_do/app/controllers/tasks_controller.rb
@@ -1,2 +1,17 @@
 class TasksController < ApplicationController
+  def index
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create!(params[:task].permit(:title, :text))
+    redirect_to @task
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
 end
