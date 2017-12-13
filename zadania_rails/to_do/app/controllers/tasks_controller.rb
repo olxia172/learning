fa92a96411b7f441f1984200646897a12cd18b63
olxia172 <kucharczyk.aleksandra@gmail.com>
2017-12-13 15:40:35 +0100

    Add edit, destroy methods

--- zadania_rails/to_do/app/controllers/tasks_controller.rb
+++ zadania_rails/to_do/app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all.order(created_at: :desc)
   end
@@ -8,11 +9,34 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.create!(params[:task].permit(:title, :text))
+    @task = Task.new(params.require(:task).permit(:title, :text))
+    @task.save
     redirect_to tasks_path
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    task_params = @task.attributes
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def find_task
     @task = Task.find(params[:id])
   end
 end
