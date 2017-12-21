0788d0eb87656b0c64f2b590ed4f73eff2067314
olxia172 <kucharczyk.aleksandra@gmail.com>
2017-12-21 16:08:09 +0100

    Create model Movie and run migration

---
+++ zadania_rails/movies_catalog/db/migrate/20171221150348_create_movies.rb
@@ -0,0 +1,16 @@
+class CreateMovies < ActiveRecord::Migration[5.1]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.date :release_date
+      t.time :length
+      t.text :description
+      t.string :types
+      t.string :director
+      t.string :writer
+      t.string :country
+
+      t.timestamps
+    end
+  end
+end
