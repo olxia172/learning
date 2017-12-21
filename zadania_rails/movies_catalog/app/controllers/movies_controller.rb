083de1096b6c91a4fb98ba90b88144c68714785f
olxia172 <kucharczyk.aleksandra@gmail.com>
2017-12-21 16:31:45 +0100

    Add action new to controller and create new view

--- zadania_rails/movies_catalog/app/controllers/movies_controller.rb
+++ zadania_rails/movies_catalog/app/controllers/movies_controller.rb
@@ -1,5 +1,14 @@
 class MoviesController < ApplicationController
   def index
+  end
 
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @article = Article.new(params.require(:movie).permit(:title, :release_date,
+      :length, :description, :types, :director, :writer, :country))
+    @article.save
   end
 end
