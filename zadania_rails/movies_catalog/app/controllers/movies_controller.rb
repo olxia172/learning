584b544457d49695959a7ef7433c688147d0e397
olxia172 <kucharczyk.aleksandra@gmail.com>
2017-12-27 17:45:58 +0100

    Add edit and update action to movies controller

--- zadania_rails/movies_catalog/app/controllers/movies_controller.rb
+++ zadania_rails/movies_catalog/app/controllers/movies_controller.rb
@@ -1,6 +1,7 @@
 class MoviesController < ApplicationController
+  before_action :find_movie, only: [:show, :edit, :update, :destroy]
   def index
-    @movies = Movie.all
+    @movies = Movie.all.order(created_at: :desc)
   end
 
   def new
@@ -9,11 +10,34 @@ class MoviesController < ApplicationController
 
   def create
     @movie = Movie.new(params.require(:movie).permit(:title, :release_date, :length, :description, :types, :director, :writer, :country))
-    @movie.save
-    redirect_to movie_path(@movie)
+    if @movie.save
+      redirect_to movie_path(@movie)
+    else
+      render 'new'
+    end
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @movie.update(movie_params)
+      redirect_to movie_path(@movie)
+    else
+      render 'edit'
+    end
+  end
+
+private
+
+  def find_movie
     @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :release_date, :length, :description, :types, :director, :writer, :country)
   end
 end
