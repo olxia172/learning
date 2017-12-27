00a481355c1e056fabe0ef0a2935db282c480fae
olxia172 <kucharczyk.aleksandra@gmail.com>
2017-12-27 19:53:39 +0100

    Correct misspelling

--- zadania_rails/movies_catalog/db/migrate/20171227170619_create_users.rb
+++ zadania_rails/movies_catalog/db/migrate/20171227170619_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.string :first_name
       t.string :last_name
       t.string :email
-      t.string :password_digset
+      t.string :password_digest
 
       t.timestamps
     end
--- zadania_rails/movies_catalog/db/schema.rb
+++ zadania_rails/movies_catalog/db/schema.rb
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 20171227170619) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.string "password_digset"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
