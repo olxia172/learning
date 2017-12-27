7f52cdf91c8d86c57be4b8705e95d2c92dbcd0bf
olxia172 <kucharczyk.aleksandra@gmail.com>
2017-12-27 18:17:53 +0100

    Run db migration with users table

--- zadania_rails/movies_catalog/db/schema.rb
+++ zadania_rails/movies_catalog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171221150348) do
+ActiveRecord::Schema.define(version: 20171227170619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 20171221150348) do
     t.string "director"
     t.string "writer"
     t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digset"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
