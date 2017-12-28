64819c85389d3a572fa63beaefa9587d264e391b
olxia172 <kucharczyk.aleksandra@gmail.com>
2017-12-28 20:01:51 +0100

    Add constraints to opinion and movie, run migration

--- zadania_rails/movies_catalog/db/schema.rb
+++ zadania_rails/movies_catalog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171227170619) do
+ActiveRecord::Schema.define(version: 20171228183940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 20171227170619) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "opinions", force: :cascade do |t|
+    t.bigint "movie_id", null: false
+    t.string "author", null: false
+    t.integer "rate", null: false
+    t.text "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["movie_id"], name: "index_opinions_on_movie_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -37,4 +47,5 @@ ActiveRecord::Schema.define(version: 20171227170619) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "opinions", "movies"
 end
